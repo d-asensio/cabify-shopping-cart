983cc9b5beb4cf0374f57747bfa2b76dfa20b1c1
David Asensio Cañas <david.asensio@icloud.com>
2020-07-22 09:30:21 +0200

    Add Thumbnail component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import React from 'react'
+import styled from 'styled-components'
+
+import { Thumbnail } from './components'
+
+const Wrapper = styled.main`
+  display: flex;
+  flex-direction: column;
+
+  background-color: #fff;
+
+  border-radius: 4px;
+`
 
 function App () {
   return (
-    <main>
-      <h1>Hello carbon-free world</h1>
-    </main>
+    <Wrapper>
+      <Thumbnail
+        src='images/thumbnails/cap.png'
+        alt='Cap'
+      />
+      <Thumbnail
+        src='images/thumbnails/mug.png'
+        alt='Mug'
+      />
+      <Thumbnail
+        src='images/thumbnails/shirt.png'
+        alt='Shirt'
+      />
+    </Wrapper>
   )
 }
 
---
+++ src/components/Thumbnail.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+const Thumbnail = styled.img`
+  width: 72px;
+  height: 72px;
+
+  border: 1px solid #cacad1;
+
+  border-radius: 4px;
+`
+
+export default Thumbnail
