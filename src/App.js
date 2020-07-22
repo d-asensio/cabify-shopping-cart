fdbaf9d1914aadafde5c2ecf20b6c01b1c99a10d
David Asensio Cañas <david.asensio@icloud.com>
2020-07-22 19:22:43 +0200

    Fix App layout

--- src/App.js
+++ src/App.js
@@ -7,16 +7,26 @@ import {
 } from './components'
 
 const Wrapper = styled.main`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+
+  padding: 40px 32px 40px 56px;
+
+  transform: translate(-50%, -50%);
+
+  width: calc(100% - 64px);
+  height: calc(100% - 64px);
+
+  max-width: 1088px;
+  max-height: 648px;
+
   display: flex;
   flex-direction: column;
 
   background-color: #fff;
 
   border-radius: 4px;
-
-  > * {
-    margin-bottom: 16px;
-  }
 `
 
 const useCounter = (initialValue = 0) => {
