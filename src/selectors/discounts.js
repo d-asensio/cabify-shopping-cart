585bfca89beb9a045dce061283fb1f5984225941
David Asensio Cañas <david.asensio@icloud.com>
2020-07-23 16:32:30 +0200

    Add getTotalDiscountsAmount selector

--- src/selectors/discounts.js
+++ src/selectors/discounts.js
@@ -1,1 +1,11 @@
+import { createSelector } from 'reselect'
+
 export const getDiscounts = ({ discounts }) => discounts
+
+export const getTotalDiscountsAmount = createSelector(
+  getDiscounts,
+  discounts => discounts.reduce(
+    (acc, discount) => acc + discount.amount,
+    0
+  )
+)
--- src/selectors/discounts.test.js
+++ src/selectors/discounts.test.js
@@ -1,4 +1,7 @@
-import { getDiscounts } from './index'
+import {
+  getDiscounts,
+  getTotalDiscountsAmount
+} from './index'
 
 describe('getDiscounts', () => {
   it('returns the right discount object from the store', () => {
@@ -16,5 +19,36 @@ describe('getDiscounts', () => {
     ).toBe(
       state.discounts
     )
+  })
+})
+
+describe('getTotalDiscountsAmount', () => {
+  it('returns the sum of all the discounts', () => {
+    const state = {
+      discounts: [
+        {
+          name: '2x1 Mug offer',
+          amount: -10.00
+        },
+        {
+          name: 'x3 Shirt offer',
+          amount: -3.00
+        }
+      ]
+    }
+
+    expect(
+      getTotalDiscountsAmount(state)
+    ).toBe(-13.00)
+  })
+
+  it('returns 0 if there are no discounts', () => {
+    const state = {
+      discounts: []
+    }
+
+    expect(
+      getTotalDiscountsAmount(state)
+    ).toBe(0)
   })
 })
