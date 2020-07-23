c70e85dbca710c84927a3ca6ef5efb044220fd46
David Asensio Cañas <david.asensio@icloud.com>
2020-07-23 14:35:26 +0200

    Add getProduct selector

--- src/selectors/product.js
+++ src/selectors/product.js
@@ -1,6 +1,17 @@
 import { createCachedSelector } from 're-reselect'
 
+const productsById = ({ productsById }) => productsById
 const productCountersById = ({ productCountersById }) => productCountersById
+
+export const getProduct = createCachedSelector(
+  productsById,
+  (_, productId) => productId,
+  (productsById, productId) => {
+    return productsById[productId]
+  }
+)(
+  (_, productId) => productId
+)
 
 export const getProductQuantity = createCachedSelector(
   productCountersById,
--- src/selectors/product.test.js
+++ src/selectors/product.test.js
@@ -1,4 +1,40 @@
-import { getProductQuantity } from './index'
+import { getProduct, getProductQuantity } from './index'
+
+describe('getProduct', () => {
+  it('gets a specific product', () => {
+    const state = {
+      productsById: {
+        TSHIRT: {
+          id: 'TSHIRT',
+          name: 'Shirt',
+          code: 'X7R2OPX',
+          price: 20.00,
+          imageSrc: 'images/thumbnails/shirt.png'
+        }
+      }
+    }
+
+    expect(
+      getProduct(state, 'TSHIRT')
+    ).toMatchObject({
+      id: 'TSHIRT',
+      name: 'Shirt',
+      code: 'X7R2OPX',
+      price: 20.00,
+      imageSrc: 'images/thumbnails/shirt.png'
+    })
+  })
+
+  it('returns undefined if the requested product counter is not defined', () => {
+    const state = {
+      productsById: {}
+    }
+
+    expect(
+      getProduct(state, 'TSHIRT')
+    ).toBe(undefined)
+  })
+})
 
 describe('getProductQuantity', () => {
   it('gets the quantity of a specific product', () => {
