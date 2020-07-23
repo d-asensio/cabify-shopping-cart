8031d8640ff496f41f03b443ae696237e7c31edf
David Asensio Cañas <david.asensio@icloud.com>
2020-07-23 12:29:37 +0200

    Add products to the initial state

--- src/reducers/initialState.js
+++ src/reducers/initialState.js
@@ -1,1 +1,27 @@
-export default {}
+export default {
+  productsById: {
+    TSHIRT: {
+      id: 'TSHIRT',
+      name: 'Shirt',
+      code: 'X7R2OPX',
+      price: 20.00
+    },
+    MUG: {
+      id: 'MUG',
+      name: 'Mug',
+      code: 'X7R2OPY',
+      price: 5.00
+    },
+    CAP: {
+      id: 'CAP',
+      name: 'Cap',
+      code: 'X7R2OPZ',
+      price: 10.00
+    }
+  },
+  productCountersById: {
+    TSHIRT: 0,
+    MUG: 0,
+    CAP: 0
+  }
+}
