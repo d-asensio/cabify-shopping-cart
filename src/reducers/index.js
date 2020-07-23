7e3060369cd232f06202627867d6cdd58dc7f874
David Asensio Cañas <david.asensio@icloud.com>
2020-07-23 16:26:05 +0200

    Change products reducers file name

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,7 @@ import { createSlice } from '@reduxjs/toolkit'
 
 import initialState from './initialState'
 
-import * as productReducers from './product'
+import * as productReducers from './products'
 
 export const { reducer, actions } = createSlice({
   name: 'shopping-cart',
