c3a85ffc1e9f457277fb11ec2ba54a79e213439b
David Asensio Cañas <david.asensio@icloud.com>
2020-07-24 11:50:41 +0200

    Move getDiscounts to a deepEqual selector

--- src/selectors/summary.js
+++ src/selectors/summary.js
@@ -1,2 +1,13 @@
-export const getDiscounts = ({ discounts }) => discounts
+import { createDeepEqualSelector } from '../utils'
+
+/**
+ * This is created as a deepEqual selector because discounts are provided by an external service as an object, so there
+ * is no other way to guess if it has changed or not. This small optimization will prevent repaints in case discounts
+ * are updated but have not changed its structure  (which is likely to happen).
+ */
+export const getDiscounts = createDeepEqualSelector(
+  ({ discounts }) => discounts,
+  discounts => discounts
+)
+
 export const getGrandTotal = ({ grandTotal }) => grandTotal
