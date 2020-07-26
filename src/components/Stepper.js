2dd63fa77122e2446577bc34cd18e24db6691860
David Asensio Cañas <david.asensio@icloud.com>
2020-07-26 13:18:06 +0200

    Fix Stepper component by using the useControlledInputNumber hook to control the input number

--- src/components/Stepper.js
+++ src/components/Stepper.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 
-import { ensureInteger } from '../utils'
+import { useControlledInputNumber } from '../hooks'
 
 const Wrapper = styled.div`
   display: flex;
@@ -51,24 +51,7 @@ const InputNumber = styled.input.attrs({
 `
 
 function Stepper ({ value, onChange, onDecrease, onIncrease }) {
-  const handleInputChange = e =>
-    onChange(e.target.value)
-
-  /**
-   * This handler is a defense against a bug on <input type="number" />, it turns out that when writing negative numbers
-   * and capturing the value through `e.target.value` the `-` symbol is represented as an empty string when there are no
-   * more digits after it. This can be problematic since the user could potentially leave that character there alone
-   * and thus the input value will become an empty string, which is not of type `number` and would lead to unexpected
-   * behaviors such as trying to sum a number to it and getting a string back (`'' + 1 = '1'`).
-   *
-   * Enforcing an integer when the blur event is triggered is the cleanest, safest, and easiest way to bypass this issue
-   * since the component could continue being unopinionated, as it does not implement any increment/decrement logic,
-   * which is yielded to the consumer, but also enforces a solid contract.
-   */
-  const handleInputBlur = e =>
-    onChange(
-      ensureInteger(e.target.value)
-    )
+  const inputProps = useControlledInputNumber({ value, onChange })
 
   return (
     <Wrapper>
@@ -77,11 +60,7 @@ function Stepper ({ value, onChange, onDecrease, onIncrease }) {
       >
         -
       </CountButton>
-      <InputNumber
-        value={value}
-        onChange={handleInputChange}
-        onBlur={handleInputBlur}
-      />
+      <InputNumber {...inputProps} />
       <CountButton
         onClick={onIncrease}
       >
@@ -89,10 +68,6 @@ function Stepper ({ value, onChange, onDecrease, onIncrease }) {
       </CountButton>
     </Wrapper>
   )
-}
-
-Stepper.defaultProps = {
-  onChange: () => {}
 }
 
 export default Stepper
---
+++ src/hooks/index.js
@@ -0,0 +1,1 @@
+export { default as useControlledInputNumber } from './useControlledInputNumber'
