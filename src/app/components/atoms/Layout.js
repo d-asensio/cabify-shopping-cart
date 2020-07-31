af731a699f30368027b7d07c9c5e2bae2125c28e
David Asensio Cañas <david.asensio@icloud.com>
2020-07-31 13:44:03 +0200

    Create Layout component

---
+++ src/app/components/atoms/Layout.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+const Layout = styled.div`
+  display: grid;
+  grid-template-columns: auto minmax(auto, 312px);
+
+  width: 100%;
+  height: 100%;
+`
+
+export default Layout
--- src/app/components/atoms/index.js
+++ src/app/components/atoms/index.js
@@ -1,6 +1,7 @@
 export { default as Button } from './Button'
 export { default as ButtonIcon } from './ButtonIcon'
 export { default as FixedAspectRatio } from './FixedAspectRatio'
+export { default as Layout } from './Layout'
 export { default as Modal } from './Modal'
 export { default as Heading } from './Heading'
 export { default as Icon } from './Icon'
